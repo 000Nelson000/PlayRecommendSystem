217e5cd15cc40a0e78913d65f796012f100a6f4a
ihong Chen <ehome4829@gmail.com>
2017-10-12 22:44:50 +0800

    03,04 implicit,learn to rank added

---
+++ 04_lightFM_rank.py
@@ -0,0 +1,178 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 12 21:23:25 2017
+
+'copy' from : data pique | Learning to rank
+http://blog.ethanrosenthal.com/2016/11/07/implicit-mf-part-2/
+"""
+
+#%% 
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import scipy.sparse as sp
+from scipy.special import expit
+import pickle
+import csv
+import copy
+import itertools
+from lightfm import LightFM
+import lightfm.evaluation
+import sys
+sys.path.append('../')
+import helpers
+
+df = pd.read_csv('./rec-a-sketch/model_likes_anon.psv',
+                 sep='|', quoting=csv.QUOTE_MINIMAL,
+                 quotechar='\\')
+df.drop_duplicates(inplace=True)
+df.head()
+
+#%% 
+# Threshold data to only include users and models with min 5 likes.
+df = helpers.threshold_interactions_df(df, 'uid', 'mid', 5, 5)
+
+# Go from dataframe to likes matrix
+# Also, build index to ID mappers.
+
+likes, uid_to_idx, idx_to_uid,\
+mid_to_idx, idx_to_mid = helpers.df_to_matrix(df, 'uid', 'mid')
+
+#likes 
+train, test, user_index = helpers.train_test_split(likes, 5, fraction=0.2)
+# remove 
+eval_train = train.copy()
+non_eval_users = list(set(range(train.shape[0])) - set(user_index)) # set diff
+
+eval_train = eval_train.tolil()
+for u in non_eval_users:
+    eval_train[u, :] = 0.0
+eval_train = eval_train.tocsr()
+
+#%%
+# =============================================================================
+# Encode side-information,(item/users' features)
+# =============================================================================
+sideinfo = pd.read_csv('./rec-a-sketch/model_feats.psv',
+                       sep='|')
+
+# Build list of dictionaries containing features 
+# and weights in same order as idx_to_mid prescribes.
+feat_dlist = [{} for _ in idx_to_mid]
+for idx, row in sideinfo.iterrows():
+#    print(idx,row)
+    feat_key = '{}_{}'.format(row.type, str(row.value).lower())
+    idx = mid_to_idx.get(row.mid)
+    if idx is not None:
+        feat_dlist[idx][feat_key] = 1
+
+feat_dlist[0] # features dictionary 
+
+from sklearn.feature_extraction import DictVectorizer
+dv = DictVectorizer()
+item_features = dv.fit_transform(feat_dlist)
+item_features # 25655x20352
+
+#%% 
+# =============================================================================
+# training
+# =============================================================================
+
+def print_log(row, header=False, spacing=12):
+    top = ''
+    middle = ''
+    bottom = ''
+    for r in row:
+        top += '+{}'.format('-'*spacing)
+        if isinstance(r, str):
+            middle += '| {0:^{1}} '.format(r, spacing-2)
+        elif isinstance(r, int):
+            middle += '| {0:^{1}} '.format(r, spacing-2)
+        elif (isinstance(r, float)
+              or isinstance(r, np.float32)
+              or isinstance(r, np.float64)):
+            middle += '| {0:^{1}.5f} '.format(r, spacing-2)
+        bottom += '+{}'.format('='*spacing)
+    top += '+'
+    middle += '|'
+    bottom += '+'
+    if header:
+        print(top)
+        print(middle)
+        print(bottom)
+    else:
+        print(middle)
+        print(top)
+
+def patk_learning_curve(model, train, test, eval_train,
+                        iterarray, user_features=None,
+                        item_features=None, k=5,
+                        **fit_params):
+    old_epoch = 0
+    train_patk = []
+    test_patk = []
+    headers = ['Epoch', 'train p@5', 'test p@5']
+    print_log(headers, header=True)
+    for epoch in iterarray:
+        more = epoch - old_epoch
+        model.fit_partial(train, user_features=user_features,
+                          item_features=item_features,
+                          epochs=more, **fit_params)
+        this_test = lightfm.evaluation.precision_at_k(model, test, train_interactions=None, k=k)
+        this_train = lightfm.evaluation.precision_at_k(model, eval_train, train_interactions=None, k=k)
+
+        train_patk.append(np.mean(this_train))
+        test_patk.append(np.mean(this_test))
+        row = [epoch, train_patk[-1], test_patk[-1]]
+        print_log(row)
+    return model, train_patk, test_patk
+
+#%% 
+# =============================================================================
+#     training 
+# =============================================================================
+
+model = lightfm.LightFM(loss='warp', random_state=2016)
+# Initialize model.
+model.fit(train, epochs=0);
+
+iterarray = range(10, 110, 10)
+
+model, train_patk, test_patk = patk_learning_curve(
+    model, train, test, eval_train, iterarray, k=5, **{'num_threads': 4}
+)
+
+#%%
+# =============================================================================
+# plot
+# =============================================================================
+import seaborn as sns
+sns.set_style('white')
+
+def plot_patk(iterarray, patk,
+              title, k=5):
+    plt.plot(iterarray, patk);
+    plt.title(title, fontsize=20);
+    plt.xlabel('Epochs', fontsize=24);
+    plt.ylabel('p@{}'.format(k), fontsize=24);
+    plt.xticks(fontsize=14);
+    plt.yticks(fontsize=14);
+
+# Plot train on left
+ax = plt.subplot(1, 2, 1)
+fig = ax.get_figure();
+sns.despine(fig);
+plot_patk(iterarray, train_patk,
+         'Train', k=5)
+
+# Plot test on right
+ax = plt.subplot(1, 2, 2)
+fig = ax.get_figure();
+sns.despine(fig);
+plot_patk(iterarray, test_patk,
+         'Test', k=5)
+
+plt.tight_layout();
+
+#%%
