015d4705e4bea044ac267fb983d1d9433c08eafc
ihong Chen <ehome4829@gmail.com>
2017-10-17 21:55:14 +0800

    naive no-features funds,recall:0.2

--- 05_funds_test.py
+++ 05_funds_test.py
@@ -7,7 +7,11 @@ Created on Tue Oct 17 14:54:01 2017
 
 #%% 
 import pandas as pd 
+import scipy.sparse as sp
 import pypyodbc 
+import numpy as np 
+from lightfm import LightFM
+from lightfm.evaluation import auc_score,recall_at_k,precision_at_k
 
 con = pypyodbc.connect("DRIVER={SQL Server};SERVER=dbm_public;UID=sa;PWD=xxxxxxx;DATABASE=project2017")
 
@@ -29,13 +33,25 @@ df_user = pd.read_sql(sql_userfeatures,con)
 df.to_csv('./funds/purchase.csv',index=False)
 df_item.to_csv('./funds/item_features.csv',index=False)
 df_user.to_csv('./funds/user_features.csv',index=False)
+
+#%%
+# =============================================================================
+# Load csv data to memory
+# =============================================================================
+
+df_inter = pd.read_csv('./funds/purchase.csv',encoding='cp950')
+df_item = pd.read_csv('./funds/item_features.csv',encoding='cp950')
+df_user = pd.read_csv('./funds/user_features.csv',encoding='cp950')
+
 #%% 
 
 # =============================================================================
 # user- item iteaction from purchasing history
 # =============================================================================
-
-
+df_gt2 = threshold_interaction(df_inter,'身分證字號','基金代碼')
+purchased_ui, userid_to_idx, \
+idx_to_userid, itemid_to_idx,idx_to_itemid = df_to_spmatrix(df_inter,'身分證字號','基金代碼')
+train,test, user_idxs = train_test_split(purchased_ui,split_count=1,fraction=0.2)
 
 #%% 
 def threshold_interaction(df,rowname,colname,row_min=1):
@@ -83,20 +99,109 @@ def df_to_spmatrix(df, rowname, colname):
     rid_to_idx = {}
     idx_to_rid = {}
     for (idx, rid) in enumerate(rids):
-        rid_to_idx[idx] = rid
-        idx_to_rid[rid] = idx
+        rid_to_idx[rid] = idx
+        idx_to_rid[idx] = rid
         
     cid_to_idx = {}
     idx_to_cid = {}
     for (idx, cid) in enumerate(cids):
-        cid_to_idx[idx] = cid
-        idx_to_cid[cid] = idx
+        cid_to_idx[cid] = idx
+        idx_to_cid[idx] = cid
         
     ### 
     
+    def map_ids(row, mapper):
+        return mapper[row]
     
+    I = df[rowname].apply(map_ids, args=[rid_to_idx]).as_matrix()
+    J = df[colname].apply(map_ids, args=[cid_to_idx]).as_matrix()
+    V = np.ones(I.shape[0])    
+    interactions = sp.coo_matrix((V,(I,J)),dtype='int32')
+    interactions = interactions.tocsr()
     
-    
-    
+    return interactions,rid_to_idx,idx_to_rid,cid_to_idx,idx_to_cid
 
 
+def train_test_split(sp_interaction, split_count, fraction=None):
+    """ split interaction data into train and test sets 
+    
+    Parameters
+    ----------
+    sp_interaction: csr sparse matrix
+    split_count: int
+        Number of u-i interaction per user to move from original sets 
+        to test set.
+    fractions: float
+        Fractions of users to split off into test sets.
+        If None, all users are considered. 
+    """
+    train = sp_interaction.copy().tocoo()
+    test = sp.lil_matrix(train.shape)  
+    
+    if fraction:
+        try:
+            user_idxs = np.random.choice(
+                np.where(np.bincount(train.row) >= split_count * 2)[0],
+                replace=False,
+                size=np.int64(np.floor(fraction * train.shape[0]))
+            ).tolist()
+        except:
+            print(('Not enough users with > {} '
+                  'interactions for fraction of {}')\
+                  .format(2*split_count, fraction))
+            raise
+    else:
+        user_idxs = range(sp_interaction.shape[0])
+        
+    train = train.tolil()
+    for uidx in user_idxs:
+        test_interactions = np.random.choice(sp_interaction.getrow(uidx).indices,
+                                        size=split_count,
+                                        replace=False)
+        train[uidx, test_interactions] = 0.
+        test[uidx, test_interactions] = sp_interaction[uidx, test_interactions]
+        
+    # Test and training are truly disjoint
+    assert(train.multiply(test).nnz == 0)
+    return train.tocsr(), test.tocsr(), user_idxs
+
+#%%
+# =============================================================================
+# train model, no- features used ( naive test ----)
+# =============================================================================
+NUM_THREADS =4
+NUM_COMPONENTS = 30
+model = LightFM(loss='warp',
+                no_components=NUM_COMPONENTS,
+                random_state=2017)
+model.fit(train,epochs=20,
+          num_threads=4,verbose=True)
+
+
+# sanity check
+# Don't forget the pass in the item features again!
+train_auc = auc_score(model,
+                      train,                      
+                      num_threads=NUM_THREADS).mean()
+train_recall = recall_at_k(model,
+                           train,
+                           num_threads=NUM_THREADS).mean()
+print('sanity check\n\tno-features used trained auc: {0:.2f}\n'.format(train_auc))
+print('\tno-features used trained recall: {0:.2f}\n'.format(train_recall))
+
+#%%
+# test check 
+test_auc = auc_score(model,
+                    test,
+#                    train_interactions=train,
+#                    item_features=item_features,
+                    num_threads=NUM_THREADS).mean()
+test_recall = recall_at_k(model,
+                          test,
+                          num_threads=NUM_THREADS).mean()
+print('TEST===\n\tauc : {0:.2f}\n'.format(test_auc))
+print('\t recall: {0:.2f}'.format(test_recall))
+
+# =============================================================================
+# grid search
+# =============================================================================
