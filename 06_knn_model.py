48ba82e74d1ab0a9320a7bb0aa457ba01544446a
ihong Sinopac <ehome4829@gmail.com>
2017-10-26 16:57:18 +0800

    add ubcf_fs

--- 06_knn_model.py
+++ 06_knn_model.py
@@ -14,7 +14,7 @@ Created on Mon Oct 23 10:41:13 2017
 class KNNmodel:
     def __init__(self,interaction,kind='ubcf'):
         
-        assert kind.lower() in ('ubcf','ibcf','popular')
+        assert kind.lower() in ('ubcf','ibcf','popular','ubcf_fs')
         self.inter = interaction
         self.kind = kind
         
@@ -75,9 +75,6 @@ class KNNmodel:
         
         for i,j,v in tqdm(zip(X_coo.row,X_coo.col,X_coo.data),
                           total = len(X_coo.data)):
-#            if (np.isnan(v)):
-#                data[idx] = 0
-#            if remove:
             if (i,j) in intersect_ij:
                 data[idx] = 0
             idx+=1
@@ -99,7 +96,7 @@ class KNNmodel:
                     
         return X_coo.tocsr()
     
-    def fit(self,topK=20,normalize=True,remove=False):
+    def fit(self,topK=20,normalize=True,remove=False,user_features=None):
         """fit model
         
         params
@@ -136,7 +133,6 @@ class KNNmodel:
 
             if normalize:
                 eps = 1e-5
-#                np.seterr(divide='ignore',invalid='ignore') # suppress warning message 
                 denominator = sim.sum(axis=1) 
                 zero_idx = np.where(np.isclose(denominator,0)) # which is zero in denominator is not allowd
                 denominator[zero_idx[0],] = np.full((len(zero_idx[0]),1),eps) 
@@ -154,7 +150,6 @@ class KNNmodel:
                 pred[:,item] = rating[:,topk_item].dot(\
                     sim[topk_item,item])
 #                pred[:,item] /= np.sum(np.abs(sim[:,item])) # extremely slow
-
             
             if normalize:
 #                np.seterr(divide='ignore',invalid='ignore') # suppress warning message 
@@ -175,8 +170,22 @@ class KNNmodel:
                 pred[user,topK_pop_indices] = topK_num_of_pop
         
         elif self.kind == 'ubcf_fs':
-            pass
+            """built CF model based on -- feature selected ubcf """
+            assert user_features.shape[0] == self.inter.shape[0]
             
+            inter_copy = self.inter.copy() # copy of interaction data
+            for idx in range(user_features.shape[1]):
+                temp_prev = inter_copy.copy()
+                rows_zeros = np.where(user_features[:,idx])[0]
+                csr_rows_set_nz_to_val(temp_prev,rows_zeros, value=0) ## set to zero 
+                model = KNNmodel(inter_copy,kind='ubcf')
+                model.jaccard_sim()
+                model.fit()
+                
+                pred+= model.rating
+            
+
+
             
         
         print('{} rating matrix built...'.format(self.kind))
@@ -345,11 +354,11 @@ if __name__ == "__main__":
     # =============================================================================
     
     # %%
-    
+        
 #%%
     
 # =============================================================================
-# test    
+# test  zone   
 # =============================================================================
 #model = model_p 
 #
@@ -370,4 +379,81 @@ if __name__ == "__main__":
 #for m in models:
 #    test_eval(m)
 #    
+    userid_to_idx
+    rowname = '身分證字號'
+    def map_ids(row, mapper):        
+        return mapper.get(row,np.nan)
+    temp = df_user.copy()
+    temp['uidx'] = df_user[rowname].apply(map_ids, args=[userid_to_idx])
+    temp
+    ## random sample user 
+    f_bool1 = np.random.randint(0,2,size=train.shape[0])
+    f_bool2 = np.random.randint(0,2,size=train.shape[0])
+    f_bool3 = np.random.randint(0,2,size=train.shape[0])
+    f_bool = np.vstack((f_bool1,f_bool2,f_bool3)).T # for features test !!
     
+    f_tables = f_bool
+    
+    inter = train.copy()
+    inter_lil = inter.tolil()
+    
+    
+    ## built CF model based on bool features infos
+    temp_inter = inter.copy()
+    model_list = []
+    pred = sp.lil_matrix((temp_inter.shape))
+    for i in range(3):
+        
+        temp_prev = temp_inter.copy()
+        rows_zeros = np.where(f_tables[:,i])[0]
+        csr_rows_set_nz_to_val(temp_prev,rows_zeros,value=0) ## set to zero 
+        model = KNNmodel(temp_prev,kind='ibcf')
+        model.jaccard_sim()
+        model.fit()
+        model_list.append(model)
+        pred += model.rating
+    
+                
+        
+    
+    
+    
+#    inter * sp.csr_matrix(f_bool2d)
+    inter_csr = inter.tocsr()
+    rows_zeros = np.where(f_bool==0)[0]
+    
+    
+    model_temp = KNNmodel(temp_inter,kind='ibcf')
+    model_temp.jaccard_sim()
+    model_temp.fit(topK=100)
+    
+        
+    
+# %%        
+
+import time
+## how to 
+def csr_row_set_nz_to_val(csr, row, value=0):
+    """Set all nonzero elements (elements currently in the sparsity pattern)
+    to the given value. Useful to set to 0 mostly.
+    """
+    if not isinstance(csr, sp.csr_matrix):
+        raise ValueError('Matrix given must be of CSR format.')
+    csr.data[csr.indptr[row]:csr.indptr[row+1]] = value
+
+def csr_rows_set_nz_to_val(csr, rows, value=0):
+    for row in rows:
+        csr_row_set_nz_to_val(csr, row)
+    if value == 0:
+        csr.eliminate_zeros()
+        
+def evaluate(size):
+    degs = [1]*size
+    inVs = list(range(1, size, int(size/25)))
+    outVs = list(range(5, size, int(size/25)))
+    where = np.hstack((inVs, outVs)).astype(int)
+    start_time = time.time()
+    A = sp.csc_matrix((size, size))
+    M = sp.diags([degs], [0]) - A
+    csr_rows_set_nz_to_val(M, where)
+    return time.time()-start_time
--- rec_helper.py
+++ rec_helper.py
@@ -167,3 +167,18 @@ def print_log(row, header=False, spacing=12):
         print(middle)
         print(top)
 
+
+## ref :https://stackoverflow.com/questions/19784868/what-is-most-efficient-way-of-setting-row-to-zeros-for-a-sparse-scipy-matrix
+def csr_row_set_nz_to_val(csr, row, value=0):
+    """Set all nonzero elements (elements currently in the sparsity pattern)
+    to the given value. Useful to set to 0 mostly.
+    """
+    if not isinstance(csr, sp.csr_matrix):
+        raise ValueError('Matrix given must be of CSR format.')
+    csr.data[csr.indptr[row]:csr.indptr[row+1]] = value
+
+def csr_rows_set_nz_to_val(csr, rows, value=0):
+    for row in rows:
+        csr_row_set_nz_to_val(csr, row)
+    if value == 0:
+        csr.eliminate_zeros()
